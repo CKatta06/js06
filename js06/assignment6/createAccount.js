e7c99ec38450f5845edc19d84abaf350de24d255
Ck <157666965+CKatta06@users.noreply.github.com>
2024-03-16 12:16:19 +0530

    Add files via upload

---
+++ js06/assignment6/createAccount.js
@@ -0,0 +1,44 @@
+const urlParams = new URLSearchParams(window.location.search);
+
+    if (urlParams.has("email") && urlParams.has("password")) {
+        console.log("Query parameters found");
+        const email = urlParams.get("email");
+        const password = urlParams.get("password");
+        const address = urlParams.get("address");
+        const city = urlParams.get("city");
+        const state = urlParams.get("state"); 
+
+        const userInfo = {
+            "Email": email,
+            "Password": password,
+            "Address": address,
+            "City": city,
+            "State":state,
+
+        };
+
+
+
+        displayUserInfo(userInfo);
+    }
+
+
+    function displayUserInfo(userInfo) {
+        console.log("Displaying user information");
+        const tableBody = document.getElementById("table-body");
+
+        for (const key in userInfo) {
+            const row = document.createElement("tr");
+    
+            const infoCell = document.createElement("td");
+            infoCell.textContent = key;
+    
+            const valueCell = document.createElement("td");
+            valueCell.textContent = userInfo[key];
+    
+            row.appendChild(infoCell);
+            row.appendChild(valueCell);
+    
+            tableBody.appendChild(row);
+        }
+    }
